7f9a1dee4b58bf461c39ecc2659f05dcfa48a4b1
Subarna Pokharel <subarnapokharel252@gmail.com>
2022-05-01 05:34:15 +0545

    Added-destroy-mechanism-after-picking-package

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {   bool hasPackage;
+    [SerializeField]float destroyDelay=0.5f;
     void OnCollisionEnter2D(Collision2D other) {
        Debug.Log("Ouch!!!");
    }
     void OnTriggerEnter2D(Collider2D other) {
-        if(other.tag=="Package"){
+        if(other.tag=="Package" && hasPackage==false){
             Debug.Log("Package been successfully picked up!!!");
             hasPackage=true;
+            Destroy(other.gameObject,destroyDelay);
         }
          if(other.tag=="Customer" && hasPackage){
             Debug.Log("Successfully delivered the package to the customer!!!");
