3362e3f68a490263212390e52324f870005c7bc7
Subarna Pokharel <subarnapokharel252@gmail.com>
2022-05-01 06:25:01 +0545

    Added-color-change-and-boost-speed-mechanism

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -5,6 +5,16 @@ using UnityEngine;
 public class Delivery : MonoBehaviour
 {   bool hasPackage;
     [SerializeField]float destroyDelay=0.5f;
+
+    [SerializeField] Color32 hasPackageColor=new Color32(1,1,1,1);
+
+    [SerializeField] Color32 noPackageColor=new Color32(1,1,1,1);
+
+    SpriteRenderer spriteRenderer;
+
+    void Start(){
+        spriteRenderer=GetComponent<SpriteRenderer>();
+    }
     void OnCollisionEnter2D(Collision2D other) {
        Debug.Log("Ouch!!!");
    }
@@ -13,10 +23,12 @@ public class Delivery : MonoBehaviour
             Debug.Log("Package been successfully picked up!!!");
             hasPackage=true;
             Destroy(other.gameObject,destroyDelay);
+            spriteRenderer.color=hasPackageColor;
         }
          if(other.tag=="Customer" && hasPackage){
             Debug.Log("Successfully delivered the package to the customer!!!");
             hasPackage=false;
+            spriteRenderer.color=noPackageColor;
 
         }
    }
--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -6,6 +6,10 @@ public class Driver : MonoBehaviour
 {
     [SerializeField]float steerSpeed=1f;
     [SerializeField]float moveSpeed=0.01f;
+
+    [SerializeField] float slowSpeed=15f;
+
+    [SerializeField] float boostSpeed=30f;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,5 +23,16 @@ public class Driver : MonoBehaviour
         transform.Rotate(0,0,-steerAmount);
         float moveAmount=Input.GetAxis("Vertical")*moveSpeed*Time.deltaTime;
         transform.Translate(0,moveAmount,0);
+    }
+
+     void OnTriggerEnter2D(Collider2D other) {
+        if(other.tag=="Boost"){
+            moveSpeed=boostSpeed;
+            Destroy(other.gameObject,0.5f);
+        }
+    }
+
+     void OnCollisionEnter2D(Collision2D other) {
+        moveSpeed=slowSpeed;
     }
 }
